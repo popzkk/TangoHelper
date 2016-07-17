7f62770358a8610d162092af2c91cb28fea48588
Kaikai Zhu <kaikaiz@kaikaiz-macbookpro.roam.corp.google.com>
2016-07-18 00:52:43 +0200

    avoid a warning

--- TangoHelper/Classes/Shared/THHelpers.h
+++ TangoHelper/Classes/Shared/THHelpers.h
@@ -7,61 +7,20 @@
 typedef void (^THBasicAlertConfirmAction)();
 typedef void (^THTextsAlertConformAction)(NSArray<UITextField *> *);
 
-static UIBarButtonItem *system_item(UIBarButtonSystemItem sys, id target, SEL action) {
-  return [[UIBarButtonItem alloc] initWithBarButtonSystemItem:sys target:target action:action];
-}
+#if __cplusplus
+extern "C" {
+#endif
 
-static UIBarButtonItem *custom_item(NSString *title, UIBarButtonItemStyle style, id target,
-                                    SEL action) {
-  return [[UIBarButtonItem alloc] initWithTitle:title style:style target:target action:action];
-}
+UIBarButtonItem *system_item(UIBarButtonSystemItem sys, id target, SEL action);
 
-static UIAlertController *basic_alert(NSString *title, NSString *message,
-                                      THBasicAlertConfirmAction block) {
-  UIAlertController *alert =
-      [UIAlertController alertControllerWithTitle:title
-                                          message:message
-                                   preferredStyle:UIAlertControllerStyleAlert];
-  [alert addAction:[UIAlertAction actionWithTitle:kCancel
-                                            style:UIAlertActionStyleDefault
-                                          handler:nil]];
-  [alert addAction:[UIAlertAction actionWithTitle:kConfirm
-                                            style:UIAlertActionStyleDefault
-                                          handler:^(UIAlertAction *action) {
-                                            block();
-                                          }]];
-  return alert;
-}
+UIBarButtonItem *custom_item(NSString *title, UIBarButtonItemStyle style, id target, SEL action);
 
-static UIAlertController *texts_alert(NSString *title, NSString *message, NSArray *texts,
-                                      NSArray *placeholders, THTextsAlertConformAction block) {
-  UIAlertController *alert =
-      [UIAlertController alertControllerWithTitle:title
-                                          message:message
-                                   preferredStyle:UIAlertControllerStyleAlert];
-  if (texts.count != placeholders.count) {
-    NSLog(@"cannot create 'texts_alert': inconsistant number of elements");
-    return nil;
-  }
-  for (NSUInteger i = 0; i < texts.count; ++i) {
-    NSString *text = texts[i];
-    [alert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
-      if (text.length == 0) {
-        textField.placeholder = placeholders[i];
-      } else {
-        textField.text = text;
-      }
-    }];
-  }
-  [alert addAction:[UIAlertAction actionWithTitle:kCancel
-                                            style:UIAlertActionStyleDefault
-                                          handler:nil]];
-  [alert addAction:[UIAlertAction actionWithTitle:kConfirm
-                                            style:UIAlertActionStyleDefault
-                                          handler:^(UIAlertAction *action) {
-                                            block(alert.textFields);
-                                          }]];
-  return alert;
-}
+UIAlertController *basic_alert(NSString *title, NSString *message, THBasicAlertConfirmAction block);
+UIAlertController *texts_alert(NSString *title, NSString *message, NSArray *texts,
+                               NSArray *placeholders, THTextsAlertConformAction block);
+
+#if __cplusplus
+}  // Extern C
+#endif
 
 #endif /* THHelpers_h */
---
+++ TangoHelper/Classes/Shared/THHelpers.m
@@ -0,0 +1,66 @@
+#import "THHelpers.h"
+
+#if __cplusplus
+extern "C" {
+#endif
+
+UIBarButtonItem *system_item(UIBarButtonSystemItem sys, id target, SEL action) {
+  return [[UIBarButtonItem alloc] initWithBarButtonSystemItem:sys target:target action:action];
+}
+
+UIBarButtonItem *custom_item(NSString *title, UIBarButtonItemStyle style, id target,
+                                    SEL action) {
+  return [[UIBarButtonItem alloc] initWithTitle:title style:style target:target action:action];
+}
+
+UIAlertController *basic_alert(NSString *title, NSString *message,
+                                      THBasicAlertConfirmAction block) {
+  UIAlertController *alert =
+  [UIAlertController alertControllerWithTitle:title
+                                      message:message
+                               preferredStyle:UIAlertControllerStyleAlert];
+  [alert addAction:[UIAlertAction actionWithTitle:kCancel
+                                            style:UIAlertActionStyleDefault
+                                          handler:nil]];
+  [alert addAction:[UIAlertAction actionWithTitle:kConfirm
+                                            style:UIAlertActionStyleDefault
+                                          handler:^(UIAlertAction *action) {
+                                            block();
+                                          }]];
+  return alert;
+}
+
+UIAlertController *texts_alert(NSString *title, NSString *message, NSArray *texts,
+                                      NSArray *placeholders, THTextsAlertConformAction block) {
+  UIAlertController *alert =
+  [UIAlertController alertControllerWithTitle:title
+                                      message:message
+                               preferredStyle:UIAlertControllerStyleAlert];
+  if (texts.count != placeholders.count) {
+    NSLog(@"cannot create 'texts_alert': inconsistant number of elements");
+    return nil;
+  }
+  for (NSUInteger i = 0; i < texts.count; ++i) {
+    NSString *text = texts[i];
+    [alert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+      if (text.length == 0) {
+        textField.placeholder = placeholders[i];
+      } else {
+        textField.text = text;
+      }
+    }];
+  }
+  [alert addAction:[UIAlertAction actionWithTitle:kCancel
+                                            style:UIAlertActionStyleDefault
+                                          handler:nil]];
+  [alert addAction:[UIAlertAction actionWithTitle:kConfirm
+                                            style:UIAlertActionStyleDefault
+                                          handler:^(UIAlertAction *action) {
+                                            block(alert.textFields);
+                                          }]];
+  return alert;
+}
+
+#if __cplusplus
+}  // Extern C
+#endif
